1b5cc787976ff57bcc701fb35cd67f308a2c0912
khlam <4841220+khlam@users.noreply.github.com>
2021-05-15 11:40:30 -0700

    11

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import sys
 print("Python Version: ", sys.version)
+
+array = [['a', 'b', 'c', 'd'], ['1', '2', '3']]
+
+out = []
+for ii, rule in enumerate(array): # a rule
+    for ij, valR, in enumerate(rule): # each element in a rule
+        out.append([valR])
+        for ik, rule2 in enumerate(array): # all other rules
+            if (ik != ii):
+                for il, valL, in enumerate(rule): # each element in all other rules
+                    out.append([valR, valL])
+                
+print(out)
